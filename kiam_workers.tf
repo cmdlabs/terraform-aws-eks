a068ea536cb0517c58c9ca61ddba6609188f1ca2
Dean Illfeld <dean.illfeld@cmdsolutions.com.au>
2019-04-09 17:53:05 +1000

    add kiam support

---
+++ kiam_workers.tf
@@ -0,0 +1,88 @@
+resource "aws_autoscaling_group" "kiam_workers" {
+  count            = "${var.enable_kiam ? 1 : 0}"
+  name_prefix      = "eks-${var.cluster_name}-kiam-workers"
+  desired_capacity = "${var.kiam_asg_desired}"
+  min_size         = "${var.kiam_asg_min}"
+  max_size         = "${var.kiam_asg_max}"
+
+  vpc_zone_identifier = ["${split(",", coalesce(var.kiam_vpc_subnets, join(",", var.private_subnets)))}"]
+
+  suspended_processes = ["${compact(split(",", var.kiam_asg_suspended_processes))}"]
+  enabled_metrics     = ["${compact(split(",", var.kiam_asg_enabled_metrics))}"]
+
+  mixed_instances_policy {
+    instances_distribution {
+      on_demand_allocation_strategy            = "${var.kiam_on_demand_allocation_strategy}"
+      on_demand_base_capacity                  = "${var.kiam_on_demand_base_capacity}"
+      on_demand_percentage_above_base_capacity = "${var.kiam_on_demand_percentage_above_base_capacity}"
+      spot_allocation_strategy                 = "${var.kiam_spot_allocation_strategy}"
+      spot_instance_pools                      = "${var.kiam_spot_instance_pools}"
+      spot_max_price                           = "${var.kiam_spot_max_price}"
+    }
+
+    launch_template {
+      launch_template_specification {
+        launch_template_id = "${aws_launch_template.kiam_workers.id}"
+        version            = "$Latest"
+      }
+
+      override {
+        instance_type = "${var.kiam_instance_type_1}"
+      }
+
+      override {
+        instance_type = "${var.kiam_instance_type_2}"
+      }
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = ["desired_capacity"]
+  }
+
+  tags = ["${concat(
+    list(
+      map("key", "Name", "value", "eks-${var.cluster_name}-workergroup-kiam", "propagate_at_launch", "true"),
+      map("key", "kubernetes.io/cluster/${var.cluster_name}", "value", "owned", "propagate_at_launch", "true"),
+      map("key", "k8s.io/cluster-autoscaler/${var.kiam_autoscaling_enabled == 1 ? "enabled" : "disabled"  }", "value", "true", "propagate_at_launch", "false")
+    )
+  )}"]
+}
+
+resource "aws_launch_template" "kiam_workers" {
+  count       = "${var.enable_kiam ? 1 : 0}"
+  name_prefix = "eks-${var.cluster_name}-kiam-workers"
+
+  image_id               = "${coalesce(var.kiam_ami_id, data.aws_ami.eks_worker.id)}"
+  instance_type          = "${var.kiam_instance_type_1}"
+  user_data              = "${base64encode(data.template_file.kiam_launch_template_userdata.rendered)}"
+  vpc_security_group_ids = ["${aws_security_group.workers.id}"]
+
+  iam_instance_profile = {
+    name = "${aws_iam_instance_profile.kiam_workers_launch_template.name}"
+  }
+
+  block_device_mappings {
+    device_name = "${data.aws_ami.eks_worker.root_device_name}"
+
+    ebs {
+      volume_size           = "${var.kiam_root_volume_size}"
+      volume_type           = "gp2"
+      delete_on_termination = true
+    }
+  }
+
+  monitoring {
+    enabled = "${var.kiam_detailed_monitoring}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_instance_profile" "kiam_workers_launch_template" {
+  role  = "${aws_iam_role.workers_kiam.name}"
+  count = "${var.enable_kiam ? 1 : 0}"
+}
--- variables.tf
+++ variables.tf
@@ -79,3 +79,113 @@ variable "cluster_endpoint_public_access" {
   description = "Enable Amazon EKS public API server endpoint."
   default     = true
 }
+
+variable "enable_kiam" {
+  description = "Create IAM roles and Nodes to be used by KIAM. Enabling this requires KIAM to be active and deployed to your cluster for IAM roles to work."
+  default     = true
+}
+
+variable "kiam_ami_id" {
+  description = "KIAM instances AMI ID"
+  default     = ""
+}
+
+variable "kiam_root_volume_size" {
+  description = "KIAM instances root volume size"
+  default     = 100
+}
+
+variable "kiam_autoscaling_enabled" {
+  description = "Allows cluster-autoscaler to manage this ASG"
+  default     = "true"
+}
+
+variable "kiam_asg_desired" {
+  description = "ASG Desired Size"
+  default     = "2"
+}
+
+variable "kiam_asg_min" {
+  description = "ASG Minimum Size"
+  default     = "2"
+}
+
+variable "kiam_asg_max" {
+  description = "ASG Maximum Size"
+  default     = "5"
+}
+
+variable "kiam_on_demand_allocation_strategy" {
+  description = "Strategy to use when launching on-demand instances"
+  default     = "prioritized"
+}
+
+variable "kiam_on_demand_base_capacity" {
+  description = "Absolute minimum amount of desired capacity that must be fulfilled by on-demand instances"
+  default     = "0"
+}
+
+variable "kiam_on_demand_percentage_above_base_capacity" {
+  description = "Percentage split between on-demand and spot instances above the base on-demand capacity"
+  default     = "0"
+}
+
+variable "kiam_spot_allocation_strategy" {
+  description = "How to allocate capacity across the Spot pools"
+  default     = "lowest-price"
+}
+
+variable "kiam_spot_instance_pools" {
+  description = "Number of Spot pools per availability zone to allocate capacity"
+  default     = 10
+}
+
+variable "kiam_spot_max_price" {
+  description = "Maximum price youre willing to pay for spot instances. Defaults to the on demand price if blank"
+  default     = ""
+}
+
+variable "kiam_instance_type_1" {
+  description = "First choice of instace type that will be used by the KIAM ASG"
+  default     = "t3.small"
+}
+
+variable "kiam_instance_type_2" {
+  description = "Second choice of instance type that will be used by the KIAM ASG"
+  default     = "t2.small"
+}
+
+variable "kiam_detailed_monitoring" {
+  description = "Enabled detailed monitoring of KIAM instances"
+  default     = false
+}
+
+variable "kiam_kubelet_extra_args" {
+  description = "Additional arguments to pass to the kubelet"
+  default     = "--node-labels=spot=true,node-role.kubernetes.io/kiam=true --register-with-taints=node-role.kubernetes.io/kiam=true:NoSchedule"
+}
+
+variable "kiam_pre_userdata" {
+  description = "Userdata to prepend to the standard userdata"
+  default     = ""
+}
+
+variable "kiam_additonal_userdata" {
+  description = "Userdata to append to the standard userdata"
+  default     = ""
+}
+
+variable "kiam_vpc_subnets" {
+  description = "A comma seperated string of subnets for the ASG to place kiam instances in"
+  default     = ""
+}
+
+variable "kiam_asg_suspended_processes" {
+  description = "A comma seperated string of ASG suspended processes"
+  default     = ""
+}
+
+variable "kiam_asg_enabled_metrics" {
+  description = "A comma seperated string of ASG enabled metrics"
+  default     = ""
+}

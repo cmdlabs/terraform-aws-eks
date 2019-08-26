6699ff2df12e2fd7ec7ff408e580b4ccd27f26f6
Fahad Tariq <fahad.tariq@cmdsolutions.com.au>
2019-08-26 23:33:16 +1000

    removed redundant rule

--- cluster.tf
+++ cluster.tf
@@ -71,17 +71,7 @@ resource "aws_security_group_rule" "master_to_worker" {
   source_security_group_id = aws_security_group.workers.id
 }
 
-resource "aws_security_group_rule" "additional_sg" {
-  count                    = length(var.cluster_access_additional_sg)
-  security_group_id        = aws_security_group.cluster.id
-  type                     = "ingress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  source_security_group_id = var.cluster_access_additional_sg[count.index]
-}
-
-resource "aws_security_group_rule" "additional_sg" {
+resource "aws_security_group_rule" "additional_sg_ingress" {
   count                    = length(var.cluster_access_additional_sg)
   security_group_id        = aws_security_group.cluster.id
   type                     = "ingress"

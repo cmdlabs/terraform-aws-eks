d50584e7411a589741893dd23feb649d7e26a3e3
Fahad Tariq <fahad.tariq@cmdsolutions.com.au>
2019-08-29 17:42:33 +1000

    removing multi rule creation

--- cluster.tf
+++ cluster.tf
@@ -82,11 +82,11 @@ resource "aws_security_group_rule" "additional_sg_ingress" {
 }
 
 resource "aws_security_group_rule" "additional_ip_ingress" {
-  count                    = length(var.cluster_access_additional_ip)
+  count                    = length(var.cluster_access_additional_ip)>0 ? 1 : 0
   security_group_id        = aws_security_group.cluster.id
   type                     = "ingress"
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  cidr_blocks               = var.cluster_access_additional_ip[count.index]
+  cidr_blocks              = var.cluster_access_additional_ip
 }
